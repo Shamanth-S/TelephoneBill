5b07e81c8f757a8b7a0c8309fb8a6b47dd88e59b
Shamanth-S <143203005+Shamanth-S@users.noreply.github.com>
2023-09-03 22:46:54 +0530

    Update TelephoneBill.java

--- src/TelephoneBill.java
+++ src/TelephoneBill.java
@@ -8,27 +8,27 @@ class TelephoneBill {
         char plan = in.next().charAt(0);
 
         System.out.print("Enter the number of minutes: ");
-        int minutes = in.nextInt();
+        int MIN = in.nextInt();
 
-        double total = calculate(plan, minutes);
+        double total = calculate(plan, MIN);
 
         System.out.println("Total bill:" + total + "Rupees");
         in.close();
     }
 
-    static double calculate(char plan, int minutes) {
+    static double calculate(char plan, int MIN) {
         double base;
         double extra = 0.0;
 
         if (plan == 'R' || plan == 'r') {
             base = 10.0;
-            if (minutes > 50) {
-                extra = (minutes - 50) * 0.2;
+            if (MIN > 50) {
+                extra = (MIN - 50) * 0.2;
             }
         } else if (plan == 'P' || plan == 'p') {
             base = 25.0;
-            if (minutes > 100) {
-                extra = (minutes - 100) * 0.1;
+            if (MIN > 100) {
+                extra = (MIN - 100) * 0.1;
             }
         } else {
             System.out.println("Invalid plan type.");
